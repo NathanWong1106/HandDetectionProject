8c0a0a1c4a1da3596d61c4324eee0c06b73534a9
Nathan Wong <56657800+NathanWong1106@users.noreply.github.com>
2021-07-01 23:27:14 -0400

    got prediction model somewhat working

--- src/Training/DatasetConstructor.py
+++ src/Training/DatasetConstructor.py
@@ -1,21 +1,27 @@
+import array
 import math
 from typing import TextIO
 
-import mediapipe
+import mediapipe as mp
 from src.Util.Detector import Detector
 import cv2
 import csv
+import pandas as pd
 
 
 def main():
-
     set_name = input("Dataset Name: ")
     append = "a" if input("Append to file (yes/no): ") == "yes" else "w"
 
     capture = cv2.VideoCapture(0)
     detector = Detector()
 
-    with open(f'{set_name}.csv', append) as file:
+    with open(f'./Data/{set_name}.csv', append, newline="") as file:
+
+        header_arr = get_std_header()
+        header_writer = csv.writer(file)
+        header_writer.writerow(header_arr)
+
         while True:
             success, img = capture.read()
             key = cv2.waitKey(1)
@@ -23,7 +29,7 @@ def main():
             # When 'c' is pressed then add the points to the dataset
             if key == ord('c'):
                 landmarks = detector.get_landmarks(img).multi_hand_landmarks
-                write_landmarks_to_dataset(img, landmarks, file)
+                write_landmarks_to_dataset(img, landmarks, file, set_name)
 
             elif key == ord('q'):
                 cv2.destroyAllWindows()
@@ -34,48 +40,64 @@ def main():
             cv2.imshow("Test", img)
 
 
-def write_landmarks_to_dataset(img, landmarks, file: TextIO):
+def get_std_header():
+    header_arr = []
+
+    for index in mp.solutions.hands.HandLandmark:
+        header_arr.append(f"x{index}")
+        header_arr.append(f"y{index}")
+    header_arr.append("gesture")
+
+    return header_arr
+
+
+def write_landmarks_to_dataset(img, landmarks, file: TextIO, gesture_name: str):
     """
     Writes the normalized and relative landmark location from the wrist to use as training data
     :param img: numpy array representing a BGR image from cv2
     :param landmarks: landmarks returned from mediapipe
     :param file: file to write to
+    :param gesture_name: gesture name
     :return: None
     """
     writer = csv.writer(file)
+
+    if landmarks is not None:
+        for hand in landmarks:
+            arr = get_normalized_relative_landmarks(img, hand)
+            arr.append(gesture_name)
+            writer.writerow(arr)
+        print("Data Written")
+
+
+def get_normalized_relative_landmarks(img, hand) -> array.ArrayType:
     height, width, channels = img.shape
     max_x, max_y = -math.inf, -math.inf
 
-    if landmarks is not None:
-        write_arr = []
-        for hand_landmark in landmarks:
-            arr = []
-            wrist_landmark = hand_landmark.landmark[mediapipe.solutions.hands.HandLandmark.WRIST]
-            wrist_coords = (wrist_landmark.x * width, wrist_landmark.y * height)
+    arr = []
+    wrist_landmark = hand.landmark[mp.solutions.hands.HandLandmark.WRIST]
+    wrist_coords = (wrist_landmark.x * width, wrist_landmark.y * height)
 
-            for index in mediapipe.solutions.hands.HandLandmark:
-                # Get the relative coordinates of each landmark from the wrist
-                wrist_relative_coords = get_relative_coord(wrist_coords,
-                                                           (hand_landmark.landmark[index].x * width,
-                                                            hand_landmark.landmark[index].y * height))
+    for index in mp.solutions.hands.HandLandmark:
+        # Get the relative coordinates of each landmark from the wrist
+        wrist_relative_coords = get_relative_coord(wrist_coords,
+                                                   (hand.landmark[index].x * width,
+                                                    hand.landmark[index].y * height))
 
-                max_x = abs(wrist_relative_coords[0]) if abs(wrist_relative_coords[0]) > max_x else max_x
-                max_y = abs(wrist_relative_coords[1]) if abs(wrist_relative_coords[1]) > max_y else max_y
+        max_x = abs(wrist_relative_coords[0]) if abs(wrist_relative_coords[0]) > max_x else max_x
+        max_y = abs(wrist_relative_coords[1]) if abs(wrist_relative_coords[1]) > max_y else max_y
 
-                arr.append(
-                    f"{wrist_relative_coords[0]}|{wrist_relative_coords[1]}")
+        arr.append(wrist_relative_coords[0])
+        arr.append(wrist_relative_coords[1])
 
-            write_arr.append(arr)
+    # Normalize values in the array by maximum absolute value of x,y respectively
+    for index, val in enumerate(arr):
+        if index % 2 == 0:
+            arr[index] = arr[index] / max_x
+        else:
+            arr[index] = arr[index] / max_y
 
-        for row, landmark in enumerate(write_arr):
-            for col, coord in enumerate(write_arr[row]):
-                string = write_arr[row][col]
-                tokens = string.split("|")
-
-                write_arr[row][col] = f"{float(tokens[0]) / float(max_x)}|{float(tokens[1]) / float(max_y)}"
-
-        writer.writerows(write_arr)
-        print("Data written")
+    return arr
 
 
 def get_relative_coord(origin: tuple, point: tuple) -> tuple:
--- src/Training/Model.py
+++ src/Training/Model.py
@@ -1,1 +1,52 @@
-# https://www.tensorflow.org/tutorials/customization/custom_training_walkthrough
+# https://www.tensorflow.org/tutorials/load_data/csv
+import cv2
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import mediapipe as mp
+import keras
+from src.Util.Detector import Detector
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from DatasetConstructor import get_normalized_relative_landmarks
+
+
+def main():
+    dataframe = pd.read_csv('./Data/concat.csv')
+
+    features = dataframe.copy()
+    labels = features.pop('gesture')
+    labels = LabelEncoder().fit_transform(labels.array)
+    features = np.array(features)
+
+    print(labels)
+
+    model = tf.keras.models.Sequential([
+        # input layer
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        # hidden layers
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        # softmax activation returns probabilities of results
+        tf.keras.layers.Dense(2, activation="softmax")
+    ])
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=['accuracy'])
+
+    model.fit(features, labels, epochs=5, batch_size=1)
+
+    # TODO: Compile and save model
+    # detector = Detector()
+    # img = cv2.imread("./Data/open_palm.jpg")
+    # hand_landmarks = detector.get_landmarks(img).multi_hand_landmarks
+    #
+    # for hand in hand_landmarks:
+    #     arr = get_normalized_relative_landmarks(img, hand)
+    #     arr = np.array(arr).reshape((1, 42))
+    #     predict = model.predict(arr).argmax()
+    #     print(predict)
+
+
+if __name__ == "__main__":
+    main()
